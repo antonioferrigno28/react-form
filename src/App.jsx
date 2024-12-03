e8cd6002add4cfac149bfba06de1f4cf14677795
antonioferrigno28 <ferrigno1@hotmail.it>
2024-12-03 13:02:35 +0100

    Base form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-// import { useState } from 'react'
+import { useState } from "react";
+
+// Esercizio
+// Creare un semplice form con un campo input per il titolo di un articolo del blog.
+// Al submit del form, mostrare la lista degli articoli inseriti.
+// Infine dare la possibilità di cancellare ciascun articolo utilizzando un'icona.
+// BONUS
+// Implementare la funzionalità di modifica del titolo di un post.
+// Aggiungere più campi al form (ad es. lo stato di un articolo - draft, published - o l’autore)
+// Buon lavoro!
 
 function App() {
   return (
     <>
       <div className="container">
-        <h1 className="mt-5">Hello World</h1>
+        <h1 className="mt-5">BoolBlog</h1>
+        <div className="wrapper mt-5">
+          <form>
+            <input type="text" />
+            <button className="ms-2">Ao</button>
+          </form>
+        </div>
       </div>
     </>
   );
