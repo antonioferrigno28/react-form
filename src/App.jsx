866f25ccf4cea927f98aad543321532cf56a0789
antonioferrigno28 <ferrigno1@hotmail.it>
2024-12-03 14:30:04 +0100

    Static posts map

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import posts from "./data/posts";
 
 // Esercizio
 // Creare un semplice form con un campo input per il titolo di un articolo del blog.
+
 // Al submit del form, mostrare la lista degli articoli inseriti.
+
 // Infine dare la possibilità di cancellare ciascun articolo utilizzando un'icona.
+
 // BONUS
 // Implementare la funzionalità di modifica del titolo di un post.
 // Aggiungere più campi al form (ad es. lo stato di un articolo - draft, published - o l’autore)
@@ -17,8 +21,14 @@ function App() {
         <div className="wrapper mt-5">
           <form>
             <input type="text" />
-            <button className="ms-2">Ao</button>
+            <button className="ms-2">Invia</button>
           </form>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <b>{post.autore}: </b>
+              {post.contenuto}
+            </li>
+          ))}
         </div>
       </div>
     </>
---
+++ src/data/posts.jsx
@@ -0,0 +1,29 @@
+const posts = [
+  {
+    id: 1,
+    autore: "Mario Arrigo",
+    contenuto: "Oggi voglio oziare",
+  },
+  {
+    id: 2,
+    autore: "Giulia Zocchio",
+    contenuto: "Oggi ho scoperto qualcosa di nuovo!",
+  },
+  {
+    id: 3,
+    autore: "Luca Assisi",
+    contenuto: "Warzonata?",
+  },
+  {
+    id: 4,
+    autore: "Anna Pela",
+    contenuto: "Preferisco CS:GO",
+  },
+  {
+    id: 5,
+    autore: "Paolo Dolo",
+    contenuto: "Qualcuno ha visto l'ultima serie TV?",
+  },
+];
+
+export default posts;
