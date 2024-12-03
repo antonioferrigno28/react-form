dcb44afcfdd8f4365d831433c63bc7d11fe34463
antonioferrigno28 <ferrigno1@hotmail.it>
2024-12-03 14:45:34 +0100

    Submit handle, input handler

--- src/App.jsx
+++ src/App.jsx
@@ -14,27 +14,51 @@ import posts from "./data/posts";
 // Buon lavoro!
 
 function App() {
+  const [addNewPost, setAddNewPost] = useState(posts);
+
+  function handleInput(e) {
+    setAddNewPost(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!addNewPost) {
+      alert("Hai lasciato un campo vuoto");
+      return;
+    }
+
+    setAddNewPost([...posts, addNewPost]);
+  }
+
   return (
     <>
       <div className="container">
         <h1 className="mt-5">BoolBlog</h1>
         <div className="wrapper mt-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="my-4">
-              <input type="text" placeholder="Inserisci il nome" />
+              <input
+                type="text"
+                placeholder="Inserisci il nome"
+                onChange={handleInput}
+              />
               <input
                 type="text"
                 className="ms-2"
                 placeholder="Inserisci il contenuto"
+                onChange={handleInput}
               />
               <button className="ms-2">Invia</button>
             </div>
           </form>
           <ul>
             {posts.map((post) => (
-              <li key={post.id}>
+              <li key={post.id} className="my-2">
                 <b>{post.autore}: </b>
                 {post.contenuto}
+                <button className="ms-2">Modifica titolo</button>
+                <button className="ms-2">Cancella</button>
               </li>
             ))}
           </ul>
